e683864259f6d1e2d4fcc5c4245896a949f9be2f
Carolyn Lam <caro_lyn_@hotmail.com>
2016-11-04 17:38:46 -0400

    update hover buttons styling and split game logic into 2 js files

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+  $("#deal-button").click(function() {
+
+    // // shuffle the deck
+    // myDeck.shuffle();
+
+    deal('player');
+    deal('dealer');
+    deal('player');
+    deal('dealer');
+
+    // disable the deal button
+    // only deal once per game
+    $("#deal-button").prop('disabled', true);
+
+    // undisable hit and stand buttons
+    $("#hit-button").prop('disabled', false);
+    $("#stand-button").prop('disabled', false);
+
+  });
+
+  $("#hit-button").click(function() {
+
+    deal('player');
+
+    // check if player busts or not
+    playerBusts()
+
+  });
+
+  $("#stand-button").click(function() {
+    getWinner();
+
+  });
+
+  $("#reset-button").click(function() {
+
+    resetGame();
+
+  });
+
+});
